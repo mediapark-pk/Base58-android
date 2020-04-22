61f6e0b3bdf1d2214024452c2e24fcc7d0aa4ba8
zaryab <zaryabshakir99@gmail.com>
2020-04-22 17:53:19 +0500

    Initial Commit

---
+++ app/src/main/java/com/mediaparkpk/base58/MainActivity.java
@@ -0,0 +1,21 @@
+package com.mediaparkpk.base58;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.mediaparkpk.base58android.Base58;
+import com.mediaparkpk.secp256k1android.Secp256K1;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
---
+++ base58android/src/main/java/com/mediaparkpk/base58android/Base58Exception.java
@@ -0,0 +1,8 @@
+package com.mediaparkpk.base58android;
+
+public class Base58Exception extends Exception {
+
+    public Base58Exception(String exception){
+        super(exception);
+    }
+}
